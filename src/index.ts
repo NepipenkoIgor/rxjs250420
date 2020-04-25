dc1f3398615fa9d0feca3b2268a17f723ee3653c
Ihor <igornepipenko@gmail.com>
2020-04-25 17:42:12 +0300

    Feature: skipLimit [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,9 @@
-import { interval, Observable, of, Subscriber, Unsubscribable } from "rxjs";
-import { filter } from "rxjs/operators";
-
-// function doNothing(source: Observable<any>) {
-//     return source;
-// }
-
-// function toText(source: Observable<any>) {
-//     return new Observable((subscriber)=>{
-//         subscriber.next('Rx JS is awesome');
-//         subscriber.complete()
-//     })
-// }
-//
-// interval(1000)
-//     .pipe(toText)
-//     .subscribe((v) => {
-//         console.log(v);
-//     })
-
-
-// const o$ = new Observable();
-// o$.source = interval(1000)
-// o$.operator = {
-//     call(subscriber: Subscriber<unknown>, source: any): Unsubscribable | Function | void {
-//         source.subscribe(subscriber)
-//     }
-// }
-//
-class DoubleSubscriber extends Subscriber<number> {
-    next(value: number): void {
-        super.next(value * 2);
-    }
-}
-
-//
-//
-// of(1, 2, 3, 4)
-//     .subscribe(new DoubleSubscriber((v) => console.log(v)))
-
-// const double = (source: Observable<any>) => {
-// //     const o$ = new Observable();
-// //     o$.source = source;
-// //     o$.operator = {
-// //         call(subscriber: Subscriber<unknown>, source: any): Unsubscribable | Function | void {
-// //             source.subscribe(new DoubleSubscriber(subscriber))
-// //         }
-// //     }
-// //     return o$;
-// // }
-
-const double = (source: Observable<any>) => {
-    return source.lift({
-        call(subscriber: Subscriber<unknown>, source: any): Unsubscribable | Function | void {
-            source.subscribe(new DoubleSubscriber(subscriber))
-        }
-    });
-}
-
-const pipe = (...fns: Function[]) => (source: Observable<any>) => fns.reduce((acc: any, fn) => {
-    return fn(acc)
-}, source);
-
-const doubleWithFilter = pipe(double, filter((v: number) => v % 3 === 0));
+import { interval } from "rxjs";
+import { skipLimit } from "./skip-limit";
 
 
 interval(1000)
-    .pipe(
-        doubleWithFilter
-    )
+    .pipe(skipLimit(4, 3))
     .subscribe((v) => {
-        console.log(v);
+        console.log(v)
     })
